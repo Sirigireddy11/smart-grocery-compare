8d97fecf4e129b268e0ade4334f8a0bad1a33513
Sirigireddy11 <harshasirigireddy11@gmail.com>
2026-06-24 12:24:11 +0000

    Deploying to gh-pages from @ Sirigireddy11/smart-grocery-compare@124b5df9dc0fa859abf81cfc89020694e8cd12df 🚀

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C="g-v2";self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"])))逼});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const C="g-v3";self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"])))逼});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
